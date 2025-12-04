668c5edac983508d38f969c41f0054abdce84076
DEV106\n.ouou <n.ouou@pem.fr>
2025-12-04 13:11:44 +0100

    Improved AI input check + Fixed image library scroll

--- Assets/Scripts/QuizMakerNew.cs
+++ Assets/Scripts/QuizMakerNew.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static QuizMakerNew;
 
 public class QuizMakerNew : MonoBehaviour
 {
